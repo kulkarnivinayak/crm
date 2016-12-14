3043011265946416ff61dc3d117271cb34c0a7a2
Vinayak Kulkarni <vinayak@shoptreeapp.com>
2016-12-15 03:53:20 +0530

    Some Minor UI changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,36 @@
 @extends('layouts.app')
     @section('content')
     <div class="container">
-      <ul class="collection">
-        <li class="collection-header"><h4>Customers</h4></li>
-        <li class="divider"></li>
-        <li class="collection-item avatar">
-          <img src="https://placehold.it/180x180" alt="" class="circle">
-          <span class="title">Title</span>
-          <p>First Line</p>
-          <a href="#!" class="secondary-content"><i class="material-icons">send</i></a>
-        </li>
-        <li class="collection-item avatar">
-          <img src="https://placehold.it/180x180" alt="" class="circle">
-          <span class="title">Title</span>
-          <p>First Line</p>
-          <a href="#!" class="secondary-content"><i class="material-icons">send</i></a>
-        </li>
-        <li class="collection-item avatar">
-          <img src="https://placehold.it/180x180" alt="" class="circle">
-          <span class="title">Title</span>
-          <p>First Line</p>
-          <a href="#!" class="secondary-content"><i class="material-icons">send</i></a>
-        </li>
-        <li class="collection-item avatar">
-          <img src="https://placehold.it/180x180" alt="" class="circle">
-          <span class="title">Title</span>
-          <p>First Line</p>
-          <a href="#!" class="secondary-content"><i class="material-icons">send</i></a>
-        </li>
-      </ul>
       <table class="highlight responsive-table">
         <thead>
           <tr>
-              <th data-field="id">Name</th>
-              <th data-field="name">Item Name</th>
-              <th data-field="price">Item Price</th>
+              <th data-field="id">Date</th>
+              <th data-field="name">Name</th>
+              <th data-field="company">Company</th>
+              <th data-field="address">Address</th>
+              <th data-field="phone">Phone</th>
+              <th data-field="email">Email</th>
+              <th data-field="referral">Referral</th>
+              <th data-field="existingPos">Existing POS</th>
+              <th data-field="notes">Notes</th>
+              <th data-field="updates">Updates</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Alvin</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-          </tr>
-          <tr>
-            <td>Alan</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-          </tr>
-          <tr>
-            <td>Jonathan</td>
-            <td>Lollipop</td>
-            <td>$7.00</td>
-          </tr>
-          <tr>
-            <td>Shannon</td>
-            <td>KitKat</td>
-            <td>$9.99</td>
+            <td>{{ $date }}</td>
+            <td>Satish Raja</td>
+            <td>Big Matka</td>
+            <td>Kormangala</td>
+            <td><a href="tel:919282388384">+91 92823 88384</a></td>
+            <td><a href="mailto:satish@bigmatka.in">satish@bigmatka.in</a></td>
+            <td>N/A</td>
+            <td>CIRQ</td>
+            <td>Lorem ipsum Ex commodo nulla Excepteur ad consectetur laboris quis sunt mollit.</td>
+            <td>Lorem ipsum Ex commodo nulla Excepteur ad consectetur laboris quis sunt mollit.</td>
           </tr>
         </tbody>
       </table>
-
-      <div class="row">
-        <div class="col s12 m6">
-          <div class="card sticky-action" style="overflow: hidden;">
-            <div class="card-image waves-effect waves-block waves-light">
-              <img class="activator" src="https://placehold.it/100x100">
-            </div>
-            <div class="card-content">
-              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-
-              <p><a href="#!">This is a link</a></p>
-            </div>
-
-            <div class="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
-            </div>
-
-            <div class="card-reveal" style="display: none; transform: translateY(0px);">
-              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-              <p>Here is some more information about this product that is only revealed once clicked on.</p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   @include('layouts.partials.footer')
 @endsection
